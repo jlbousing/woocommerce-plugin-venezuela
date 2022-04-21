628054e4bbb4a6c545c6f48a4366bb6d806da70a
jorge <jbousing@gmail.com>
2022-04-21 19:56:06 -0400

    init plugin

---
+++ woocommerce-plugin-venezuela.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Woocommerce Plugin Venezuela
+Plugin URI: https://www.jorgelbou-saad.com
+Description: Plugin para registrar pagos de pedidos con Bolivares a traves de Pago movil, transferencias bancarias y zelle
+Version: 1.0.o
+Author: Jorge Luis Bou-saad
+Author URI: https://www.jorgelbou-saad.com
+License: GPLv2
+*/
