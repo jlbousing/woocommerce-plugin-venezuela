77793a8777258f4404e08cdadaec27d2632b8927
jorge <jbousing@gmail.com>
2022-04-26 00:48:31 -0400

    se corrige error de llamadas de archivos php

--- woocommerce-plugin-venezuela.php
+++ woocommerce-plugin-venezuela.php
@@ -9,18 +9,22 @@ Author URI: https://www.jorgelbou-saad.com
 License: GPLv2
 */
 
+
+//SE INICIALIZA PLUGIN CON DATOS QUE DEBEN ESTAR DISPONIBLES EN BASE DE DATOS (BANCOS)
+require_once plugin_dir_path(__FILE__) . "/src/ActivatePlugin.php";
+
+
 add_filter( 'woocommerce_payment_gateways', 'test_add_gateway_class' );
 function test_add_gateway_class( $gateways ) {
     $gateways[] = 'WC_Test_Gateway'; // your class name is here
     return $gateways;
 }
 
-
 add_action( 'plugins_loaded', 'init_gateway_class' );
 function init_gateway_class() {
 
     //require_once plugin_dir_path( __FILE__ ) . '/src/WC_Test_Gateway.php';
-    require_once plugin_dir_path(__FILE__) . "src/WC_Pago_Movil_Venezuela";
-    require_once plugin_dir_path(__FILE__) . "src/WC_Transferencias_Venezuela";
-    require_once plugin_dir_path(__FILE__) . "src/WC_Zelle_Venezuela";
+    require_once plugin_dir_path(__FILE__) . "src/WC_Pago_Movil_Venezuela.php";
+    require_once plugin_dir_path(__FILE__) . "src/WC_Transferencias_Venezuela.php";
+    require_once plugin_dir_path(__FILE__) . "src/WC_Zelle_Venezuela.php";
 }
